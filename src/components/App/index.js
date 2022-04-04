39d967ea33313946195c80c75420a91cb259be2a
Justine-Gouaux <justine.gouaux93@gmail.com>
2022-04-04 11:17:14 +0200

    [build]-add-layouts:footer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,11 +2,13 @@
 import './styles.scss';
 
 import Header from 'src/components/Header';
+import Footer from 'src/components/Footer';
 
 // == Composant
 const App = () => (
   <div className="app">
     <Header />
+    <Footer />
   </div>
 );
 
