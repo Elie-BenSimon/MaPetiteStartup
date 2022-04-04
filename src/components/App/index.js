d916b3ae9bf5e9d69a22d8a7a67c14edb1d85fe7
Justine-Gouaux <justine.gouaux93@gmail.com>
2022-04-04 16:12:27 +0200

    [build]-add-layouts:navbar

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,11 +3,13 @@ import './styles.scss';
 
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
+import NavBar from 'src/components/NavBar';
 
 // == Composant
 const App = () => (
   <div className="app">
     <Header />
+    <NavBar />
     <Footer />
   </div>
 );
