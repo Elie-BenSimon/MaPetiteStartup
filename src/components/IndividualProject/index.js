6f157f7e8174a47e4bf4827bfed53a81585f18fe
Elie-BenSimon <eliebensimon_2@hotmail.com>
2022-04-05 11:42:38 +0200

    [feat]-add dynamic availble dev list on individual project page

--- src/components/IndividualProject/index.js
+++ src/components/IndividualProject/index.js
@@ -1,7 +1,6 @@
 // == Imports
 import { Navigate, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import ProjectCard from 'src/components/Projects/ProjectCard';
 
 // == Component
 const IndividualProject = () => {
@@ -9,13 +8,46 @@ const IndividualProject = () => {
   const projectsList = useSelector((state) => state.project.projectsList);
   const project = projectsList.find((p) => p.id == id);
 
+  const devsList = useSelector((state) => state.dev.devList);
+  const availableDevsList = devsList.filter((d) => d.code_project === null);
+  const onProjectDevsList = devsList.filter((d) => d.code_project === id);
+  console.log(devsList, availableDevsList);
+
   if (!project) {
     return <Navigate to="/projects" replace />;
   }
 
   return (
     <div className="individualProject">
-      <ProjectCard {...project} />
+      <h2>{project.name}</h2>
+      <div className="individualProject__infos">
+        <p>Niveau de difficulté</p>
+        <p>{project.difficulty}</p>
+        <p>Récompenses</p>
+        <p>50000$</p>
+        <p>250 pts de réputation</p>
+        <p>Avancement</p>
+        <p>{project.completion}</p>
+      </div>
+
+      <div className="individualProject__team">
+        <div className="individualProject__team__devs">
+          <h2>Equipe sur le projet</h2>
+          <ul>
+            <li>dev1</li>
+            <li>dev2</li>
+            <li>dev3</li>
+          </ul>
+        </div>
+        <div className="individualProject__team__availableDevs">
+          <h2>Developpeurs disponibles</h2>
+          <ul>
+            {availableDevsList.map((dev) => (
+              <li>{dev.name}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
