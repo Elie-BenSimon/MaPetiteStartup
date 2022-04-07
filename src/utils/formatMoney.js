b0380df0c9690474861d425ae9761aeffce46a4c
Elie-BenSimon <eliebensimon_2@hotmail.com>
2022-04-07 22:17:50 +0200

    [feat]-update-formatMoney

--- src/utils/formatMoney.js
+++ src/utils/formatMoney.js
@@ -2,10 +2,10 @@ const formatMoney = (initialValue) => {
   let unit = '';
   let value = initialValue;
 
-  if (value >= 100000) {
+  if (value >= 1000) {
     unit = 'K';
     value = Math.round(value / 1000);
-    if (value >= 100000000) {
+    if (value >= 1000000) {
       unit = 'M';
       value = Math.round(value / 1000000);
     }
