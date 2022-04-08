e6edad1d3ec4f2f3fa1d247b8b53248ceaec7a29
Justine-Gouaux <justine.gouaux93@gmail.com>
2022-04-08 18:02:06 +0200

    [refacto] - add error page content

--- src/components/Statics/Error/index.js
+++ src/components/Statics/Error/index.js
@@ -1,9 +1,23 @@
 // == Imports
+import './error.scss';
+
+import stop from 'src/assets/img/stop.gif';
+
+import { Link } from 'react-router-dom';
 
 // == Component
 const Error = () => (
-  <div>
-    <p>Pas par là!</p>
+  <div className="error">
+    <h2>404</h2>
+    <h3>C'est pas par là!</h3>
+    <img src={stop} alt="Pixelled developper crossing arms" />
+    <p>
+      Le retour dans le droit chemin c'est
+      <Link className="error__link" to="/">
+        par ici
+      </Link>
+      !
+    </p>
   </div>
 
 );
