2f3f59084f72a103916651bd9bc7d1471c1730e7
Justine-Gouaux <justine.gouaux93@gmail.com>
2022-04-08 12:01:46 +0200

    [refacto] - add rules content

--- src/components/Statics/Legals/index.js
+++ src/components/Statics/Legals/index.js
@@ -13,9 +13,9 @@ const Legals = () => (
       <h3 className="legals__content__title">
         Mention légales
       </h3>
-      <h4>Objectifs de Ma petite startup</h4>
+      <h4>Objectifs de <span className="bold">Ma petite startup</span></h4>
       <p>
-        Ma Petite Startup est un site de jeu en ligne à visée ludique.
+        <span className="bold">Ma petite startup</span> est un site de jeu en ligne à visée ludique.
       </p>
 
       <h4>Collecte et utilisation des données</h4>
@@ -24,7 +24,7 @@ const Legals = () => (
         Directive Européenne du 24 octobre 1995.
       </p>
       <p>
-        Ma Petite Startup ne recueille uniquement que l'adresse email de l'utilisateur.rice nécessaire à la création de son compte de joueur.se, et utilisée uniquement à cette fin.
+        <span className="bold">Ma petite startup</span> ne recueille uniquement que l'adresse email de l'utilisateur.rice nécessaire à la création de son compte de joueur.se, et utilisée uniquement à cette fin.
       </p>
       <p>
         Celle-ci est stockée de manière sécurisée durant toute la durée d'activitée du compte de joueur.se. Elle n’est ni publiée à l’insu de l’utilisateur.rice, ni échangée, transférée, cédée ou vendue sur un support quelconque à des tiers.
@@ -32,7 +32,7 @@ const Legals = () => (
 
       <h4>Origines des illustrations</h4>
       <p>
-        Toutes les illustrations présentes sur le site de Ma petite startup sont des images libres de droits.
+        Toutes les illustrations présentes sur le site de <span className="bold">Ma petite startup</span> sont des images libres de droits.
       </p>
       <p>
         Elles sont issues de
@@ -57,10 +57,10 @@ const Legals = () => (
         Conditions générales d'utilisation
       </h3>
       <p>
-        L'utilisation de termes insultants ou discrimants est formellement interdite sur Ma Petite Startup.
+        L'utilisation de termes insultants ou discrimants est formellement interdite sur <span className="bold">Ma petite startup</span>.
       </p>
       <p>
-        Les administateur.rices.s de Ma Petite Starup se réservent le droit de supprimer le compte d'un.e utilisateur.rice si les conditions générales d'utilisation sus-indiquées ne sont pas respectées.
+        Les administateur.rices.s de <span className="bold">Ma petite startup</span> se réservent le droit de supprimer le compte d'un.e utilisateur.rice si les conditions générales d'utilisation sus-indiquées ne sont pas respectées.
       </p>
       <p>
         Toutefois, les administateur.rices.s ne peuvent pas êtres tenu.e.s pour responsables d'un comportement de joueur inadéquat.
