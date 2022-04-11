1ee1ad9716c271b892ab509377d8662701b2adec
Elie-BenSimon <eliebensimon_2@hotmail.com>
2022-04-11 11:55:35 +0200

    [feat]-update cost display

--- src/components/Startup/index.js
+++ src/components/Startup/index.js
@@ -4,6 +4,7 @@ import formatMoney from 'src/utils/formatMoney';
 import { useSelector } from 'react-redux';
 
 const Startup = ({ totalSalary }) => {
+  const rent = useSelector((state) => state.startup.rent);
   const totalPlaces = useSelector((state) => state.dev.totalPlaces);
   const totalPlacesArray = [];
   for (let i = 0; i < totalPlaces; i += 1) {
@@ -30,20 +31,26 @@ const Startup = ({ totalSalary }) => {
           </div>
           <button className="button button--change-location" type="button">Déménager</button>
         </div>
+        <p>loyer: {rent}$/mois</p>
         <h3>Mon équipe</h3>
-        <ul className="startup__team">
-          {devList.map((dev) => (
-            <li className="startup__team__dev" key={dev.id}>
-              <img src={`https://avatars.dicebear.com/api/human/${dev.avatar}.svg`} alt="" className="avatar card__avatar" />
-              <div className="">
-                <p>{dev.name}</p>
-                <p>{dev.salary}$/mois</p>
-                <p>lassitude: {dev.lassitude}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
-        <p>Total des salaires: {formatMoney(totalSalary)}$/mois</p>
+        <div className="startup__team">
+          <ul className="startup__team__list">
+            {devList.map((dev) => (
+              <li className="startup__team__list__dev" key={dev.id}>
+                <img src={`https://avatars.dicebear.com/api/human/${dev.avatar}.svg`} alt="" className="avatar card__avatar" />
+                <div className="">
+                  <p>{dev.name}</p>
+                  <p>{dev.salary}$/mois</p>
+                  <p>lassitude: {dev.lassitude}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <p>Total des salaires: {formatMoney(totalSalary)}$/mois</p>
+          <p>Total des charges: {totalSalary + rent}$/mois</p>
+        </div>
+        <h3>Mes projets</h3>
+
       </div>
     </div>
   );
