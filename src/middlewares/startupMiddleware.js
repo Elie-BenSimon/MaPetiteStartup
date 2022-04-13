7609fb832321bedb8d0a349ceaacb19a47bc1dcb
Elie-BenSimon <eliebensimon_2@hotmail.com>
2022-04-13 10:40:50 +0200

    [fix]--update axios header fixed

--- src/middlewares/startupMiddleware.js
+++ src/middlewares/startupMiddleware.js
@@ -14,18 +14,29 @@ const startupMiddleware = (store) => (next) => (action) => {
     // startup creation
     case CREATE_STARTUP:
       console.log(`Bearer ${store.getState().user.token}`);
+      console.log(
+        store.getState().startup.name,
+        store.getState().startup.slogan,
+        store.getState().startup.logo,
+        store.getState().user.userId,
+        store.getState().startup.rent,
+        store.getState().dev.totalPlaces,
+      );
+
       axios.post(
         'http://f-gahery-server.eddi.cloud/projet-08-ma-petite-startup-back/public/api/startup',
         {
-          headers: {
-            Authorization: `Bearer ${store.getState().user.token}`,
-          },
           name: store.getState().startup.name,
           slogan: store.getState().startup.slogan,
           logo: store.getState().startup.logo,
           user_id: store.getState().user.userId,
           rent: store.getState().startup.rent,
           places: store.getState().dev.totalPlaces,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${store.getState().user.token}`,
+          },
         },
       )
         .then((response) => {
